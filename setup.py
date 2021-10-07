610345b9b7efecded0c414006589debbb9bcfdcb
chrdre <christerdr@outlook.com>
2021-10-07 15:04:25 +0200

    Setup file complete

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as infile:
+    long_description = infile.read()
+
+setuptools.setup(
+    name='lammps_logfile_reader',
+    version='0.5',
+    author='Christer Dreierstad',
+    author_email='christerdr@outlook.com',
+    description='Package for reading LAMMPS logfiles',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/chdre/lammps-logfile-reader',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=['pandas', 'regex'],
+    include_package_data=True,
+)
